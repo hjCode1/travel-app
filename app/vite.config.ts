413ee5b8b810020a05cb23b780d0d6c12689f295
hyuckjin12 <43224608+hjCode1@users.noreply.github.com>
2024-01-09 13:52:15 +0900

    feat: 프로젝트 생성

---
+++ app/vite.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      dts: true,
+      imports: ['vue', 'vue-router', '@vueuse/core'],
+      dirs: ['./src/composables', './src/composables/**'],
+      vueTemplate: false,
+    }),
+    Components({
+      dts: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
